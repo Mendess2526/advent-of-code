8682ed7a223a21ccaf7ce8e2fb5d7dc1dc6bf86f
Mendess2526 <pedro.mendes.26@gmail.com>
2019-12-07 01:51:29 +0000

    clean day4

--- 2019/day4/src/two.rs
+++ 2019/day4/src/two.rs
@@ -1,51 +1,37 @@
 const INPUT: (usize, usize) = (246515, 739105);
 
-fn break_num(i: usize) -> [u8; 6] {
-    [
-        ((i / 100000) % 10) as u8,
-        ((i / 10000) % 10) as u8,
-        ((i / 1000) % 10) as u8,
-        ((i / 100) % 10) as u8,
-        ((i / 10) % 10) as u8,
-        ((i / 1) % 10) as u8,
-    ]
-}
-
-trait Also
-where
-    Self: Sized,
-{
-    fn also<F>(mut self, f: F) -> Self
-    where
-        F: FnOnce(&mut Self),
-    {
-        f(&mut self);
-        self
-    }
-}
-
-impl<T> Also for T {}
-
 fn main() {
     println!(
         "{}",
         (INPUT.0..INPUT.1)
-            .map(break_num)
+            .map(|i| [
+                ((i / 100000) % 10) as u8,
+                ((i / 10000) % 10) as u8,
+                ((i / 1000) % 10) as u8,
+                ((i / 100) % 10) as u8,
+                ((i / 10) % 10) as u8,
+                ((i / 1) % 10) as u8,
+            ])
             .filter(|i| {
                 i.iter()
                     .skip(1)
                     .scan(0, |last, &x| {
-                        Some(x == i[*last])
+                        let r = Some(x == i[*last])
                             .map(|rep| rep && (*last == 0 || i[*last] != i[*last - 1]))
-                            .map(|rep| rep && (*last >= 4 || x != i[*last + 2]))
-                            .also(|_| *last = *last + 1)
+                            .map(|rep| rep && (*last >= 4 || x != i[*last + 2]));
+                        *last += 1;
+                        r
                     })
                     .any(|b| b)
             })
             .filter(|i| {
                 i.iter()
                     .skip(1)
-                    .scan(0, |last, x| Some(*x >= i[*last]).also(|_| *last = *last + 1))
+                    .scan(0, |last, x| {
+                        let r = Some(*x >= i[*last]);
+                        *last += 1;
+                        r
+                    })
                     .all(|b| b)
             })
             .count()
